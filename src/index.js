a52dd216bcb4230239d6498cf513492cb9855ef1
Ellina <ellinasereda18@gmail.com>
2019-03-23 10:50:02 +0300

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
+  let replacement='';
+  let n = 0 ;
+  while (n < bracketsConfig.length) {
+    n = 0;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let br = bracketsConfig[i][0] + bracketsConfig[i][1];
+      replacement = str.replace(br, "");
+      if (str == replacement) {
+        n++;
+      }
+      str = replacement;
+    }
+  }
+  return str.length == 0;
   // your solution
 }
